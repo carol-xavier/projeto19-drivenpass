5f41985513d001fbb47c219f65678d51ce2e413f
carolxavier <xavier.biotec@gmail.com>
2022-07-21 18:36:30 -0300

    tests for auth routes implemented

--- projeto19-drivenpass/src/index.ts
+++ projeto19-drivenpass/src/index.ts
@@ -11,7 +11,4 @@ app.use(json());
 app.use(router);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`)
-});
+export default app;
