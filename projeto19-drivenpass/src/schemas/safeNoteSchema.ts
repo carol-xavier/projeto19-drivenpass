5f5d0366d3ba462a2d8038c6071f37bf50f1dcd8
Guga <contato.gustavobarbosa@outlook.com>
2022-08-04 14:52:46 -0300

    Preparando revisão de código

--- projeto19-drivenpass/src/schemas/safeNoteSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-import { CreateNoteData } from "../services/safeNoteService.js";
-
-export const noteSchema = Joi.object<CreateNoteData>({
-    title: Joi.string().max(50).required(),
-    note: Joi.string().max(1000).required()
-  });
-  
--- projeto19-drivenpass/src/schemas/wifiSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-import { CreateWiFiData } from "../services/wifiService.js";
-
-export const wifiSchema = Joi.object<CreateWiFiData>({
-    wifiNickname: Joi.string().required(),
-    wifiName: Joi.string().required(),
-    password: Joi.string().required()
-  });
